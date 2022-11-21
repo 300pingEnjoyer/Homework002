acd7c0f4ebcdd079a280edc6636e40aa7d0c9183
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-22 06:18:52 +0700

    task 001 complete

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,24 +1,15 @@
-﻿Console.WriteLine("input first number");
-int num1 = Convert.ToInt32(Console.ReadLine());
+﻿// 456, 782, 918 найти второе число
 
-Console.WriteLine("input second number");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("input third number");
-int num3 = Convert.ToInt32(Console.ReadLine());
-
-//                0     1    2
-int [] array = {num1, num2, num3};
-
-int n = array.Length;
-int find = num2;
-int index = 0;
-while (index < n)
+int SecondDigit (int num)
 {
-    if (array[index] == find)
-    {
-        Console.Write("second number = ");
-        Console.WriteLine(num2);
-    }
-    index++;
+    int ed = num % 10;
+    int dec = num / 10;
+    int sot = num / 100;
+    int res = dec % 10;
+    return res;
 }
+
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int NewNumber = SecondDigit(num);
+Console.WriteLine($"New version of {num} is {NewNumber}");
