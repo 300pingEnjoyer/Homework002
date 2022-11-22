4df3245ef9901c1653065be0da3cf9befb57e01e
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-22 08:49:40 +0700

    task 003 complete

---
+++ 003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (num <= 5)
+{
+    Console.WriteLine("рабочий день");
+    result = num; 
+}
+else
+{
+    Console.WriteLine("выходной день");
+    result = num;
+}   
