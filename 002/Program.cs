755948d4141e5ef5409ef0ea0e7805bfa9eadf89
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-22 08:03:05 +0700

    task 002 complete

---
+++ 002/Program.cs
@@ -0,0 +1,22 @@
+﻿int ThirdDigit (int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    int sot = num / 100;
+    int result = -1;
+    if (num >= 100)
+{
+        while (num > 1000)
+    {
+        num = num / 10;
+    }
+    result = num % 10;
+}
+    return result;
+}
+
+
+Console.WriteLine("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int NewNumber = ThirdDigit(number);
+Console.WriteLine($"New version of {number} is {NewNumber}");
